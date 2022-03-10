b8605f077d30f5eac596a3d661672e0145de9320
Learning_Pie <100119999+LearningPie@users.noreply.github.com>
2022-03-10 03:54:44 -0800

    Frontend updated
    1} Admin Navbar
    2} Admin Home
    3} Usernavbar
    4} Frontpage
    5}Footer
    All this things are added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import { Route, Routes } from 'react-router-dom'
 import Login from './components/Login'
 import Register from './components/Register'
+import AdminHome from './components/layout/AdminHome'
+import Footer from './components/layout/footer'
+import Frontpage from './components/Frontpage'
 
 export default function app() {
   return (
     <>
       <Routes>
-        <Route path='/' element={<Login />} />
         <Route path='/Register' element={<Register />} />
         <Route path='/Login' element={<Login />} />
+        <Route path='/Admin' element={<AdminHome />} />
+        <Route path='/' element={<Frontpage />} />
       </Routes>
     </>
   )
